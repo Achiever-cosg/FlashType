f67118c60cc218b80b0cee3993d8b6aa50fc755d
Samshul Ajam <93369157+Achiever-cosg@users.noreply.github.com>
2023-03-18 12:22:54 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return(
+        <div className="footer-container">
+            <a
+                href="https://github.com/Achiever-cosg"
+                className="footer-link"
+                target="_blank"
+                rel="noreferrer"
+            >
+                Follow me on Github
+            </a>
+        </div>
+    )
+}
+
+export default Footer;
